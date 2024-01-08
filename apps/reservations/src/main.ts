5a2dead90f2d03e6e5fbd11afb5c492cbb30e7c9
DaEnV <120397126+daenv@users.noreply.github.com>
2024-01-08 23:30:57 +0700

    Add microservices and cookie parser to reservations service

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -2,12 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { ReservationsModule } from './reservations.module';
 import { Logger } from 'nestjs-pino';
 import { ValidationPipe } from '@nestjs/common';
+import { Transport } from '@nestjs/microservices';
+import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
    console.log('Starting reservations service...');
    const app = await NestFactory.create(ReservationsModule);
+   app.connectMicroservice({
+      transport: Transport.TCP
+   })
+   app.use(cookieParser())
    app.useGlobalPipes(new ValidationPipe());
    app.useLogger(app.get(Logger));
-   await app.listen(3000);
+   const configService = app.get(ConfigService)
+   await app.startAllMicroservices();
+   await app.listen(configService.get('PORT'));
 }
 bootstrap();
