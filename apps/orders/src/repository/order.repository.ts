ff01b8848ad887747491f050752651582fc0b98c
daenv <daenvle69@gmail.com>
2024-01-25 23:58:18 +0700

    Fix bug in login functionality

---
+++ apps/orders/src/repository/order.repository.ts
@@ -0,0 +1,18 @@
+import { AbstractRepository } from '@app/common/database/abstract.repository';
+import { Injectable, Logger } from '@nestjs/common';
+import { Order } from '../schema/orders.schema';
+import { InjectConnection, InjectModel } from '@nestjs/mongoose';
+import { Connection, Model } from 'mongoose';
+
+@Injectable()
+export class OrdersRepository extends AbstractRepository<Order> {
+  protected readonly logger = new Logger(OrdersRepository.name);
+
+  constructor(
+    @InjectModel(Order.name) orderModel: Model<Order>,
+    @InjectConnection() connection: Connection,
+  ) {
+    //@ts-expect-error
+    super(orderModel, connection);
+  }
+}
