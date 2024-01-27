e3ef44eccd0638cf312e06b669db2ae80ff5b170
daenv <daenvle69@gmail.com>
2024-01-27 07:45:50 +0700

    Add dependencies and update repository constructor

--- apps/orders/src/repository/order.repository.ts
+++ apps/orders/src/repository/order.repository.ts
@@ -1,14 +1,18 @@
 import { AbstractRepository } from '@app/common/database/abstract.repository';
 import { Injectable, Logger } from '@nestjs/common';
 import { Order } from '../schema/orders.schema';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { InjectConnection, InjectModel } from '@nestjs/mongoose';
+import { Connection, Model } from 'mongoose';
 
 @Injectable()
 export class OrdersRepository extends AbstractRepository<Order> {
   protected readonly logger = new Logger(OrdersRepository.name);
 
-  constructor(@InjectModel(Order.name) orderModel: Model<Order>) {
-    super(orderModel);
+  constructor(
+    @InjectModel(Order.name) orderModel: Model<Order>,
+    @InjectConnection() connection: Connection,
+  ) {
+    //@ts-ignore
+    super(orderModel, connection);
   }
 }
