94886438002e1b480c7cc490333d9348fc5b970c
daenv <daenvle69@gmail.com>
2024-01-29 10:03:36 +0700

    Add createUser endpoint to UsersController

--- apps/auth/src/users/users.controller.ts
+++ apps/auth/src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserRequest } from './dto/create-user.dto';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth/users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async createUser(@Body() request: CreateUserRequest) {
+    return this.usersService.createUser(request);
+  }
+}
