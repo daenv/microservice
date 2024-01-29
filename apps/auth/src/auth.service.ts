9aa96f05b641b02a9d25bad248288df79dab1455
daenv <daenvle69@gmail.com>
2024-01-29 09:33:25 +0700

    Add JwtService to AuthService constructor and add login method

--- apps/auth/src/auth.service.ts
+++ apps/auth/src/auth.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-
+import { JwtService } from '@nestjs/jwt';
 @Injectable()
 export class AuthService {
- constructor(private readonly configService: ConfigService, private readonly jwtService: ){} 
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {}
+  async login() {}
 }
