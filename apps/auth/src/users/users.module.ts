0f6e37ea88e0e67dd7e59e6fab34edcd6f7259d5
daenv <daenvle69@gmail.com>
2024-01-29 10:11:50 +0700

    Refactor users module to use UsersRepository and MongooseModule

--- apps/auth/src/users/users.module.ts
+++ apps/auth/src/users/users.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
+import { UsersRepository } from './repository/users.repository';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from '../schemas/user.schema';
 
 @Module({
-  providers: [UsersService],
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
   controllers: [UsersController],
+  providers: [UsersService, UsersRepository],
+  exports: [UsersService],
 })
 export class UsersModule {}
